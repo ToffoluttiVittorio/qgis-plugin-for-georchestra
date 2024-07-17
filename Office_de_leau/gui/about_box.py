84be9477232406d91bbebdd4f6f070c1b46750e0
ToffoluttiVittorio <vittorio.toffolutti@gmail.com>
2024-07-17 16:11:43 +0400

    bug

--- Office_de_leau/gui/about_box.py
+++ Office_de_leau/gui/about_box.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from qgis.PyQt.QtWidgets import QWidget, QDialog, QVBoxLayout, QLabel, QTextBrowser, QFrame
+from qgis.PyQt.QtWidgets import QWidget, QDialog, QVBoxLayout, QLabel, QTextEdit, QFrame
 from qgis.PyQt.QtGui import QPixmap
 
 from Office_de_leau.utils.plugin_globals import PluginGlobals
@@ -23,18 +23,17 @@ class AboutBox(QDialog):
         mainLayout.addWidget(self.logo)
 
 
-        title = u"À propos de l'extension Office de l'eau Réunion …"
-        description1 = """Extension pour QGIS donnant un accès simplifié aux ressources géographiques de l'Office de l'eau Réunion. Version {}<br>""".format(PluginGlobals.instance().PLUGIN_VERSION)
-        description2 = """Plus d'informations à l'adresse suivante :<br><a href='{0}'>{0}</a><br>""".format(PluginGlobals.instance().PLUGIN_SOURCE_REPOSITORY)
-        description3 = """Merci aux créateurs des plugins <a href="https://github.com/geobretagne/qgis-plugin">Géobretagne</a> et <a href="https://github.com/geograndest/qgis-plugin">GéoGrandEst</a> et <a href="https://gitlab.in2p3.fr/letg/indigeo-for-qgis">Indigeo</a>sur lesquels ce plugin est basé !"""
+        title = u"À propos de l'extension Office de l'eau Réunion…"
+        description = u"""Extension pour QGIS donnant un accès simplifié aux ressources géographiques utiles à l'Office de l'eau Réunion
+Version {0}
+Plus d'informations à l'adresse suivante :
+{1}
+        """.format(PluginGlobals.instance().PLUGIN_VERSION,
+            PluginGlobals.instance().PLUGIN_SOURCE_REPOSITORY)
 
-        self.textArea = QTextBrowser()
-#        self.textArea.setTextInteractionFlags(Qt.LinksAccessibleByMouse)
-        self.textArea.setOpenExternalLinks(True)
+        self.textArea = QTextEdit()
         self.textArea.setReadOnly(True)
-        self.textArea.setHtml(description1)
-        self.textArea.append(description2)
-        self.textArea.append(description3)
+        self.textArea.setText(description)
         self.textArea.setFrameShape(QFrame.NoFrame)
         mainLayout.addWidget(self.textArea)
 
@@ -43,5 +42,5 @@ class AboutBox(QDialog):
 
         self.setLayout(mainLayout)
 
-        self.setFixedSize(500, 350)
+        self.setFixedSize(400, 250)
         self.setWindowTitle(title)
